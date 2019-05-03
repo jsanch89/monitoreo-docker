c03f26206f1dbe585b7f6d1d6072e90855381757
root <root@ip-172-31-95-105.ec2.internal>
2019-05-03 02:42:52 +0000

    Commit nginx 2

--- web_app/ceiba-eafit/src/app/services/services_settings.ts
+++ web_app/ceiba-eafit/src/app/services/services_settings.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 const CONFIG ={
-apiUrl:'http://express:3000' // aqui se cambia por la url de heroku
+apiUrl:'http://52.206.230.227:3000' // aqui se cambia por la url de heroku
 };
 
 @Injectable()
